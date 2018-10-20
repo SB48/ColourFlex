452cc58b664d9cbf8076c1d1e097a88d9dce05e5
adam-able <32619067+adam-able@users.noreply.github.com>
2018-10-20 16:02:17 +0100

    json object parser

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,43 @@
-document.getElementById("submit").addEventListener("click", function(){
+function getColourNames(data) {
+	var colourNames = new Array();
+	
+	for (i = 0; i < 6; i++) {
+		colourNames[i] = data.tags[i].label;
+	}
+	return colourNames;
+}
+
+function getColourValues(data) {
+	var colourValues = new Array();
+	for (i = 0; i < 6; i++) {
+		colourValues[i] = data.tags[i].color;
+	}
+	return colourValues;
+}
+
+function updateColours(colourNamesArray, colourValuesArray) {
+	for (i = 0; i < colourNamesArray.length; i++) {
+		var elem = document.createElement('div');
+		
+		if (i%2 == 0) {
+			elem.className += 'colours-left';
+		} else {
+			elem.className += 'colours-right';
+		}
+		
+		console.log(colourValuesArray[i]);
+		elem.style.cssText = 'position:absolute;width:100%;height:20%;z-index:100;background:' + colourValuesArray[i];
+		
+		
+		document.body.appendChild(elem);
+	}
+}
+
+
+document.getElementById("submit").addEventListener("click", function(){   
 
 	var request = new XMLHttpRequest();
 	var text = document.getElementById('textfield').value;
-	console.log(text);
 	var url = 'https://apicloud-colortag.p.mashape.com/tag-url.json?url=' + text;
 
 	request.open('GET', url, true);
@@ -10,50 +45,19 @@ document.getElementById("submit").addEventListener("click", function(){
 	request.setRequestHeader("X-Mashape-Host", "apicloud-colortag.p.mashape.com");
 
 
-	// [[42, 41, 48], [90, 83, 84], [191, 157, 175], [188, 138, 125], [215, 170, 66]]
-	// note that the input colors have changed as well, by a small amount
 
 	request.onload = function() {
 			// Begin accessing JSON data here
 			var data = JSON.parse(this.response);
+			
+			var names = getColourNames(data);
+			var values = getColourValues(data);
+
+			updateColours(names, values);
+			
 			console.log(data);
-		}
-		request.send();
+	}
 
-			var url2 = "http://colormind.io/api/";
-			var dataInput = {
-				model : "default",
-				input : [[44,43,44],[90,83,82],"N","N","N"]
-			}
-
-			var http = new XMLHttpRequest();
-
-			http.onreadystatechange = function() {
-				if(http.readyState == 4 && http.status == 200) {
-					var palette = JSON.parse(http.responseText).result;
-				}
-			}
-
-			//https://media.boingboing.net/wp-content/uploads/2018/02/button-gone.jpg
-
-			http.open("POST", url2, true);
-			http.send(JSON.stringify(dataInput));
-
-			var data2 = JSON.parse(JSON.stringify(dataInput));
-			console.log(data2);
-
+	request.send();
 
 });
-
-
-
-function hexToRgb(hex) {
-var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-			r: parseInt(result[1], 16),
-			g: parseInt(result[2], 16),
-			b: parseInt(result[3], 16)
-	} : null;
-}
-
-alert( hexToRgb("#0033ff").g ); // "51";
