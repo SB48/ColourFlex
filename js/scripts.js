2bb33cb9d3387a41cb7fb7f1d90a27019c5d9abc
adam-able <32619067+adam-able@users.noreply.github.com>
2018-10-20 14:18:44 +0100

    gets user entered url and gets matching colour

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
+document.getElementById("submit").addEventListener("click", function(){   
+
+	var request = new XMLHttpRequest();
+	var text = document.getElementById('textfield').value;
+	console.log(text);
+	var url = 'https://apicloud-colortag.p.mashape.com/tag-url.json?url=' + text;
+
+	request.open('GET', url, true);
+	request.setRequestHeader("X-Mashape-Key", "jFhD0TiArsmshNSteVQKuPcNs2dGp1CY7tZjsnBLu38ZKsIBrY");
+	request.setRequestHeader("X-Mashape-Host", "apicloud-colortag.p.mashape.com");
 
 
-var request = new XMLHttpRequest();
 
-request.open('GET', 'https://apicloud-colortag.p.mashape.com/tag-url.json?url=http://apicloud.me/assets/colortag/image1.jpg', true);
-request.setRequestHeader("X-Mashape-Key", "jFhD0TiArsmshNSteVQKuPcNs2dGp1CY7tZjsnBLu38ZKsIBrY");
-request.setRequestHeader("X-Mashape-Host", "apicloud-colortag.p.mashape.com");
+	request.onload = function() {
+			// Begin accessing JSON data here
+			var data = JSON.parse(this.response);
 
+			//data.forEach(movie => {
+			  // Log each movie's title
+			  //console.log(movie.title);
+			//});
+			console.log(data);
+	}
 
+	request.send();
 
-request.onload = function() {
-		// Begin accessing JSON data here
-		var data = JSON.parse(this.response);
-
-		//data.forEach(movie => {
-		  // Log each movie's title
-		  //console.log(movie.title);
-		//});
-		console.log(data);
-}
-
-request.send();
-
+});
