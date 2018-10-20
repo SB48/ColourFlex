30f5a6b6d5d889f71d4b0acd00de203cbbe85360
Sophie Biber <biberwood@gmail.com>
2018-10-20 16:04:51 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,17 @@ function updateColours(colourNamesArray, colourValuesArray) {
 	}
 }
 
+function hexToRgb(hex) {
+var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16)
+	} : null;
+}
+
+alert( hexToRgb("#0033ff").g ); // "51";
+
 
 document.getElementById("submit").addEventListener("click", function(){   
 
@@ -82,3 +93,7 @@ document.getElementById("submit").addEventListener("click", function(){
 	console.log(data2);
 
 });
+
+
+
+
