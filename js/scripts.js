1099356ddefe6f34d411e20b14b687525c14fb23
adam-able <32619067+adam-able@users.noreply.github.com>
2018-10-20 16:25:52 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
+
 function getColourNames(data) {
+
+	
 	var colourNames = new Array();
 	
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 4; i++) {
 		colourNames[i] = data.tags[i].label;
 	}
 	return colourNames;
 }
 
 function getColourValues(data) {
+	
 	var colourValues = new Array();
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 4; i++) {
 		colourValues[i] = data.tags[i].color;
 	}
 	return colourValues;
@@ -25,24 +29,13 @@ function updateColours(colourNamesArray, colourValuesArray) {
 			elem.className += 'colours-right';
 		}
 		
-		console.log(colourValuesArray[i]);
+		console.log(colourNamesArray[i] + " " + colourValuesArray[i]);
 		elem.style.cssText = 'position:absolute;width:100%;height:20%;z-index:100;background:' + colourValuesArray[i];
 		
 		
 		document.body.appendChild(elem);
 	}
 }
-
-function hexToRgb(hex) {
-var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-			r: parseInt(result[1], 16),
-			g: parseInt(result[2], 16),
-			b: parseInt(result[3], 16)
-	} : null;
-}
-
-alert( hexToRgb("#0033ff").g ); // "51";
 
 
 document.getElementById("submit").addEventListener("click", function(){   
@@ -60,6 +53,7 @@ document.getElementById("submit").addEventListener("click", function(){
 	request.onload = function() {
 			// Begin accessing JSON data here
 			var data = JSON.parse(this.response);
+			console.log(data);
 			
 			var names = getColourNames(data);
 			var values = getColourValues(data);
@@ -70,30 +64,5 @@ document.getElementById("submit").addEventListener("click", function(){
 	}
 
 	request.send();
-	
-	var url2 = "http://colormind.io/api/";
-	var dataInput = {
-		model : "default",
-		input : [[44,43,44],[90,83,82],"N","N","N"]
-	}
-
-	var http = new XMLHttpRequest();
-
-	http.onreadystatechange = function() {
-		if(http.readyState == 4 && http.status == 200) {
-			var palette = JSON.parse(http.responseText).result;
-		}
-	}
-	
-	
-	http.open("POST", url2, true);
-	http.send(JSON.stringify(dataInput));
-
-	var data2 = JSON.parse(JSON.stringify(dataInput));
-	console.log(data2);
 
 });
-
-
-
-
